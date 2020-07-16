57f705a9340971870935485bb37bdef630c8ca53
ruijadom <ruijadomingues@gmail.com>
2020-07-16 20:47:24 +0100

    bottom menu

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
-import { Container, Header, BackIcon, ProfileInfo} from './styles';
+import {
+  Container,
+  Header,
+  BackIcon,
+  ProfileInfo,
+  BottomMenu,
+  HomeIcon,
+  SearchIcon,
+  BellIcon,
+  EmailIcon,
+} from './styles';
 
 const Main: React.FC = () => {
   return (
@@ -14,18 +24,19 @@ const Main: React.FC = () => {
           <strong>Rui Domingues</strong>
           <span>10 Tweets</span>
         </ProfileInfo>
+      </Header>
 
         {/* <ProfilePage /> */}
 
-        {/* <BottomMenu>
-          <HomeIcon />
+      <BottomMenu>
+          <HomeIcon className="active"/>
           <SearchIcon />
           <BellIcon />
           <EmailIcon />
-        </BottomMenu> */}
-      </Header>
+        </BottomMenu>
+
     </Container>
-  )
-}
+  );
+};
 
 export default Main;
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,6 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components';
 
-import { ArrowLeft } from '../../styles/Icons';
+import {
+  ArrowLeft,
+  Home,
+  Search,
+  Notifications,
+  Email,
+} from '../../styles/Icons';
 
 export const Container = styled.div`
   display: flex;
@@ -22,7 +28,7 @@ export const Header = styled.div`
   display: flex;
   align-items: center;
   text-align: left;
-  
+
   padding: 8px 0 9px 13px;
   border-bottom: 1px solid var(--outline);
 
@@ -45,5 +51,60 @@ export const BackIcon = styled(ArrowLeft)`
 `;
 
 export const ProfileInfo = styled.div`
+  margin-left: 17px;
+  display: flex;
+  flex-direction: column;
 
+  > strong {
+    font-size: 19px;
+  }
+  > span {
+    font-size: 15px;
+    color: var(--gray);
+  }
+`;
+
+export const BottomMenu = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+
+  background: var(--primary);
+  width: 100%;
+  border-top: 1px solid var(--outline);
+
+  display: flex;
+  justify-content: space-between;
+  padding: 8px min(46px, max(10vw, 10px));
+
+  @media (min-width: 500px) {
+    display: none;
+  }
+`;
+
+const iconCSS = css`
+  width: 31px;
+  height: 31px;
+  cursor: pointer;
+
+  fill: var(--gray);
+
+  &:hover,
+  &.active {
+    fill: var(--twitter);
+  }
+`;
+
+export const HomeIcon = styled(Home)`
+  ${iconCSS}
+`;
+export const SearchIcon = styled(Search)`
+  ${iconCSS}
+`;
+export const BellIcon = styled(Notifications)`
+  ${iconCSS}
+`;
+export const EmailIcon = styled(Email)`
+  ${iconCSS}
 `;
